5bff5e3762d082de8c3d36f172543b1f7e4a9ede
jamescross813 <crossjames813@gmail.com>
2023-09-28 14:45:32 -0500

    setup

---
+++ sum_values.py
@@ -0,0 +1,7 @@
+# Write your sum_values function here:
+
+# Uncomment these function calls to test your sum_values function:
+#print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+#print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6

7923bf472154eb38115592b00f4136e4b3097ad1
jamescross813 <crossjames813@gmail.com>
2023-09-29 15:12:56 -0500

    loop through dictionary and add sum

--- sum_values.py
+++ sum_values.py
@@ -1,8 +1,11 @@
 # Write your sum_values function here:
 def sum_values(dict):
-    pass
+    sum = 0
+    for key, val in dict.items():
+        sum+=val
+    return sum
 # Uncomment these function calls to test your sum_values function:
-#print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
-#print(sum_values({10:1, 100:2, 1000:3}))
+# print(sum_values({10:1, 100:2, 1000:3}))
 # should print 6

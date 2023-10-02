43e244b558c77f79bb7e79a0b0048591bf8c2227
jamescross813 <crossjames813@gmail.com>
2023-10-01 21:07:36 -0500

    done

--- even_keys.py
+++ even_keys.py
@@ -6,7 +6,7 @@ def sum_even_keys(dict):
             sum+=value 
     return sum
 # Uncomment these function calls to test your  function:
-# print(sum_even_keys({1:5, 2:2, 3:3}))
+print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
-# print(sum_even_keys({10:1, 100:2, 1000:3}))
+print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6

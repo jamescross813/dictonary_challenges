bc0afc4b99d15b927522c78016b18a1414d0dd89
jamescross813 <crossjames813@gmail.com>
2023-10-01 21:05:00 -0500

    setup

---
+++ even_keys.py
@@ -0,0 +1,7 @@
+# Write your sum_even_keys function here:
+
+# Uncomment these function calls to test your  function:
+#print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+#print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6

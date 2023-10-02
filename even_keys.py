44e91d5c82c83a378a0eba31375e907f2a1590e5
jamescross813 <crossjames813@gmail.com>
2023-10-01 21:07:08 -0500

    loop

--- even_keys.py
+++ even_keys.py
@@ -1,8 +1,11 @@
 # Write your sum_even_keys function here:
 def sum_even_keys(dict):
-    pass
+    sum = 0
+    for key, value in dict.items():
+        pass 
+    return sum
 # Uncomment these function calls to test your  function:
-#print(sum_even_keys({1:5, 2:2, 3:3}))
+# print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
-#print(sum_even_keys({10:1, 100:2, 1000:3}))
+# print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6

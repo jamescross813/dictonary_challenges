e18a4688a3ba186aefe060de3dcb24084a256bf8
jamescross813 <crossjames813@gmail.com>
2023-10-02 22:07:44 -0500

    setup

---
+++ values_keys.py
@@ -0,0 +1,7 @@
+# Write your values_that_are_keys function here:
+
+# Uncomment these function calls to test your  function:
+#print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+#print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]

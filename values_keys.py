13cdf9bbbc87228c9e1f49d52a3c4ac1e40781da
jamescross813 <crossjames813@gmail.com>
2023-10-02 22:08:29 -0500

    param added

--- values_keys.py
+++ values_keys.py
@@ -1,5 +1,5 @@
 # Write your values_that_are_keys function here:
-def values_that_are_keys():
+def values_that_are_keys(dict):
     pass
 # Uncomment these function calls to test your  function:
 #print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))

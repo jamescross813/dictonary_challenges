6b787f8c9f06302cb98b9c1b1e932566ccd53e1d
jamescross813 <crossjames813@gmail.com>
2023-10-02 22:08:10 -0500

    func def

--- values_keys.py
+++ values_keys.py
@@ -1,5 +1,6 @@
 # Write your values_that_are_keys function here:
-
+def values_that_are_keys():
+    pass
 # Uncomment these function calls to test your  function:
 #print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]

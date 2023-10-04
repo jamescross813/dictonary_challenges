bd357fb5ca8997fc21f5c5aa6cc6c926b4e697c6
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:41:25 -0500

    empty list added

--- values_keys.py
+++ values_keys.py
@@ -1,6 +1,6 @@
 # Write your values_that_are_keys function here:
 def values_that_are_keys(dict):
-    pass
+    list_of_value_keys =[]
 # Uncomment these function calls to test your  function:
 #print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]

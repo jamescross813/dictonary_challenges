e5f537d386b2332916067d56de380a76589202f9
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:42:09 -0500

    for loop set up

--- values_keys.py
+++ values_keys.py
@@ -1,6 +1,8 @@
 # Write your values_that_are_keys function here:
 def values_that_are_keys(dict):
     list_of_value_keys =[]
+    for values in dict.values():
+        pass
 # Uncomment these function calls to test your  function:
 #print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]

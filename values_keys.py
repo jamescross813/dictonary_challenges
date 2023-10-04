3db67135835b9aab0f7fd074ea4043033d453513
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:44:56 -0500

    if statement added to loop

--- values_keys.py
+++ values_keys.py
@@ -1,8 +1,9 @@
 # Write your values_that_are_keys function here:
 def values_that_are_keys(dict):
     list_of_value_keys =[]
-    for values in dict.values():
-        pass
+    for value in dict.values():
+        if dict.has_key(value):
+            pass
 # Uncomment these function calls to test your  function:
 #print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]

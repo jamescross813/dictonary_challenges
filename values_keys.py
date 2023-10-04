74aec0d22c9554d6d9231a0928d82ec64b106ecc
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:47:32 -0500

    done

--- values_keys.py
+++ values_keys.py
@@ -8,5 +8,5 @@ def values_that_are_keys(dict):
 # Uncomment these function calls to test your  function:
 print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]
-#print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
 # should print ["a"]

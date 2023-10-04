4ff10babe2d1d7a6e5955c71ef78439b233825f8
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:54:48 -0500

    aded in var redefinitions

--- largest_value.py
+++ largest_value.py
@@ -4,9 +4,11 @@ def max_key(dict):
     starting_value = 0
     for key, value in dict.items():
         if value > starting_value:
-            pass
+            starting_value=value
+            starting_key=key
+    return starting_key
 # Uncomment these function calls to test your  function:
-#print(max_key({1:100, 2:1, 3:4, 4:10}))
+# print(max_key({1:100, 2:1, 3:4, 4:10}))
 # should print 1
-#print(max_key({"a":100, "b":10, "c":1000}))
+# print(max_key({"a":100, "b":10, "c":1000}))
 # should print "c"

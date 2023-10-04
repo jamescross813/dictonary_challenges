f27d29fbb0e9b7ae339fac4a2cca986cbee94ff2
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:48:22 -0500

    setup

---
+++ largest_value.py
@@ -0,0 +1,7 @@
+# Write your max_key function here:
+
+# Uncomment these function calls to test your  function:
+#print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+#print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"

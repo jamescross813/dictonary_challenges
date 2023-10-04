c7e42354101ece6ce072d69a0b0bdd979fbdff0a
jamescross813 <crossjames813@gmail.com>
2023-10-03 21:47:22 -0500

    fixed issue with if statement

--- values_keys.py
+++ values_keys.py
@@ -2,10 +2,11 @@
 def values_that_are_keys(dict):
     list_of_value_keys =[]
     for value in dict.values():
-        if dict.has_key(value):
-            pass
+        if value in dict:
+            list_of_value_keys.append(value)
+    return list_of_value_keys
 # Uncomment these function calls to test your  function:
-#print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]
 #print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
 # should print ["a"]

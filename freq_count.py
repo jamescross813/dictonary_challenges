2808f94632fb49c9046c81e6a2b547f387ae6e46
jamescross813 <crossjames813@gmail.com>
2023-10-04 21:57:34 -0500

    setup

---
+++ freq_count.py
@@ -0,0 +1,7 @@
+# Write your frequency_dictionary function here:
+
+# Uncomment these function calls to test your  function:
+#print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+#print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}

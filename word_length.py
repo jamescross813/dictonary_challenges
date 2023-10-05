9e13a31dc8a081a1499d8770701920ef73890251
jamescross813 <crossjames813@gmail.com>
2023-10-04 21:56:36 -0500

    done

--- word_length.py
+++ word_length.py
@@ -5,7 +5,7 @@ def word_length_dictionary(list_of_strings):
         new_dict.update({s: len(s)})
     return new_dict
 # Uncomment these function calls to test your  function:
-# print(word_length_dictionary(["apple", "dog", "cat"]))
+print(word_length_dictionary(["apple", "dog", "cat"]))
 # should print {"apple":5, "dog": 3, "cat":3}
-#print(word_length_dictionary(["a", ""]))
+print(word_length_dictionary(["a", ""]))
 # should print {"a": 1, "": 0}

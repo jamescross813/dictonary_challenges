495a0c317f33754146d696b806d0ec9efe08929c
jamescross813 <crossjames813@gmail.com>
2023-10-04 21:56:13 -0500

    update dict with new key vcalue pair

--- word_length.py
+++ word_length.py
@@ -1,10 +1,11 @@
 # Write your word_length_dictionary function here:
 def word_length_dictionary(list_of_strings):
     new_dict = {}
-    for string in list_of_strings:
-        pass
+    for s in list_of_strings:
+        new_dict.update({s: len(s)})
+    
 # Uncomment these function calls to test your  function:
-#print(word_length_dictionary(["apple", "dog", "cat"]))
+# print(word_length_dictionary(["apple", "dog", "cat"]))
 # should print {"apple":5, "dog": 3, "cat":3}
 #print(word_length_dictionary(["a", ""]))
 # should print {"a": 1, "": 0}

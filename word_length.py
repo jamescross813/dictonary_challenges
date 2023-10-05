4679bdf59f3aca97e48e9c79f759da3ed7147b00
jamescross813 <crossjames813@gmail.com>
2023-10-04 21:56:26 -0500

    return statement added

--- word_length.py
+++ word_length.py
@@ -3,7 +3,7 @@ def word_length_dictionary(list_of_strings):
     new_dict = {}
     for s in list_of_strings:
         new_dict.update({s: len(s)})
-    
+    return new_dict
 # Uncomment these function calls to test your  function:
 # print(word_length_dictionary(["apple", "dog", "cat"]))
 # should print {"apple":5, "dog": 3, "cat":3}

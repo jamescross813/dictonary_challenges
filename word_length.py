4c185890a26c161d78f0cf1f940ce19745ef5c2c
jamescross813 <crossjames813@gmail.com>
2023-10-04 21:51:14 -0500

    func defined

--- word_length.py
+++ word_length.py
@@ -1,5 +1,6 @@
 # Write your word_length_dictionary function here:
-
+def word_length_dictionary():
+    pass
 # Uncomment these function calls to test your  function:
 #print(word_length_dictionary(["apple", "dog", "cat"]))
 # should print {"apple":5, "dog": 3, "cat":3}

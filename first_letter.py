7d7b35c328137e80519204fa69d5a9d596c7df17
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:11:18 -0500

    conditional statment

--- first_letter.py
+++ first_letter.py
@@ -3,7 +3,8 @@ def count_first_letter(dict):
     new_dict={}
     for name in dict:
         first_letter = name[0]
-        
+        if first_letter not in new_dict:
+            pass
 # Uncomment these function calls to test your  function:
 print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 4, "L": 3}

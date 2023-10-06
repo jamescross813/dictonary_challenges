402a8b3c4fcda0bddd3936e2323f015c44932532
jamescross813 <crossjames813@gmail.com>
2023-10-05 21:42:39 -0500

    done

--- freq_count.py
+++ freq_count.py
@@ -7,7 +7,7 @@ def frequency_dictionary(list_of_strings):
         new_dict[string]+=1
     return new_dict
 # Uncomment these function calls to test your  function:
-#print(frequency_dictionary(["apple", "apple", "cat", 1]))
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
 # should print {"apple":2, "cat":1, 1:1}
-#print(frequency_dictionary([0,0,0,0,0]))
+print(frequency_dictionary([0,0,0,0,0]))
 # should print {0:5}

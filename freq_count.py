1131498700647027e80c9b4fa3e3dad5b0fff36f
jamescross813 <crossjames813@gmail.com>
2023-10-05 21:41:54 -0500

    update dict to inc string and freq

--- freq_count.py
+++ freq_count.py
@@ -3,7 +3,9 @@ def frequency_dictionary(list_of_strings):
     new_dict={}
     for string in list_of_strings:
         if string not in new_dict:
-            pass
+            new_dict.update({string:0})
+        new_dict[string]+=1
+    
 # Uncomment these function calls to test your  function:
 #print(frequency_dictionary(["apple", "apple", "cat", 1]))
 # should print {"apple":2, "cat":1, 1:1}

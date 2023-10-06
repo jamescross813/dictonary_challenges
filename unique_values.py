9366ff1ceb6d8a278b4ef32491f9491cc516d08d
jamescross813 <crossjames813@gmail.com>
2023-10-05 21:45:03 -0500

    for loop

--- unique_values.py
+++ unique_values.py
@@ -1,6 +1,7 @@
 # Write your unique_values function here:
 def unique_values(dict):
-    pass
+    for val in dict.items():
+        pass
 # Uncomment these function calls to test your  function:
 #print(unique_values({0:3, 1:1, 4:1, 5:3}))
 # should print 2

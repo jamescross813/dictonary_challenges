2a86938ca61e823fb8514ec1ba69c6dc5f12f1fd
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:10:46 -0500

    var for first letter

--- first_letter.py
+++ first_letter.py
@@ -2,9 +2,10 @@
 def count_first_letter(dict):
     new_dict={}
     for name in dict:
-        pass
+        first_letter = name[0]
+        
 # Uncomment these function calls to test your  function:
-#print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 4, "L": 3}
 #print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 7}

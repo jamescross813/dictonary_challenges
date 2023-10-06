4e5a1eed859bf4f6d431dc5dff215f815d97dce6
jamescross813 <crossjames813@gmail.com>
2023-10-05 21:43:22 -0500

    setup

---
+++ unique_values.py
@@ -0,0 +1,7 @@
+# Write your unique_values function here:
+
+# Uncomment these function calls to test your  function:
+#print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+#print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1

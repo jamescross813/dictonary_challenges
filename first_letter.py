8463630a9c326ffc6109c3869883f39bae9cfd3c
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:09:16 -0500

    for loop opened

--- first_letter.py
+++ first_letter.py
@@ -1,7 +1,8 @@
 # Write your count_first_letter function here:
 def count_first_letter(dict):
     new_dict={}
-    
+    for name in dict:
+        pass
 # Uncomment these function calls to test your  function:
 #print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 4, "L": 3}

a5f50e3919c7701b100d07532c4d630f1bde5096
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:18:53 -0500

    dne

--- first_letter.py
+++ first_letter.py
@@ -10,5 +10,5 @@ def count_first_letter(dict):
 # Uncomment these function calls to test your  function:
 print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 4, "L": 3}
-#print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 7}

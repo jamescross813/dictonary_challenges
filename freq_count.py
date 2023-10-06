5f70f134fb65784a4b42742afd972f93ed3919b1
jamescross813 <crossjames813@gmail.com>
2023-10-05 21:42:07 -0500

    dict return value

--- freq_count.py
+++ freq_count.py
@@ -5,7 +5,7 @@ def frequency_dictionary(list_of_strings):
         if string not in new_dict:
             new_dict.update({string:0})
         new_dict[string]+=1
-    
+    return new_dict
 # Uncomment these function calls to test your  function:
 #print(frequency_dictionary(["apple", "apple", "cat", 1]))
 # should print {"apple":2, "cat":1, 1:1}

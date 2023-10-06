9ef55e78bb39c0977a1c40f65aa367c8dc0c134c
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:18:22 -0500

    setting first letter to key in dict if not there

--- first_letter.py
+++ first_letter.py
@@ -4,7 +4,9 @@ def count_first_letter(dict):
     for name in dict:
         first_letter = name[0]
         if first_letter not in new_dict:
-            pass
+            new_dict[first_letter] = 0
+        
+    return new_dict
 # Uncomment these function calls to test your  function:
 print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 4, "L": 3}

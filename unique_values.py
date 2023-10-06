e36b9cb21b86a85c12e6cc9411262d4523ccd9cd
jamescross813 <crossjames813@gmail.com>
2023-10-05 21:47:13 -0500

    empty list var addeD:

--- unique_values.py
+++ unique_values.py
@@ -1,9 +1,10 @@
 # Write your unique_values function here:
 def unique_values(dict):
+    my_list =[]
     for val in dict.items():
         pass
 # Uncomment these function calls to test your  function:
-#print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# print(unique_values({0:3, 1:1, 4:1, 5:3}))
 # should print 2
 #print(unique_values({0:3, 1:3, 4:3, 5:3}))
 # should print 1

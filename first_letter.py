455cdfc7162be5e9fb4cc1c99313ba667e69d922
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:18:39 -0500

    incrementing value

--- first_letter.py
+++ first_letter.py
@@ -5,7 +5,7 @@ def count_first_letter(dict):
         first_letter = name[0]
         if first_letter not in new_dict:
             new_dict[first_letter] = 0
-        
+        new_dict[first_letter] += len(dict[name])
     return new_dict
 # Uncomment these function calls to test your  function:
 print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))

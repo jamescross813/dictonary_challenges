62c67c287f264ece7c2d9520762c009572e2c61f
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:07:26 -0500

    func def inc param

--- first_letter.py
+++ first_letter.py
@@ -1,5 +1,6 @@
 # Write your count_first_letter function here:
-
+def count_first_letter(dict):
+    pass
 # Uncomment these function calls to test your  function:
 #print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
 # should print {"S": 4, "L": 3}
